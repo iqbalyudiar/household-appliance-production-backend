5f24a5cc32a234acf2f6c1301f41732676ad8ca7
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-06 09:00:44 +0700

    chore: adjust main and app module, and add new dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { UserSchema } from './users/schemas/user.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
