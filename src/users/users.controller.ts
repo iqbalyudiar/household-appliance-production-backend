4b429daf27d3da144931acc91d8cbc5bd4cb6fc8
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-09 10:08:53 +0700

    feat: add me routing on user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Headers } from '@nestjs/common';
+import { UsersService } from './users.service';
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  
+  @Get('me')
+  async register(@Headers('authorization') auth: string) {
+    return this.usersService.getMyProfile(auth);
+  }
+}
