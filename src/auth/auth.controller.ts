aa12e7bc6b0a7bf2db9b3a6b71bc13b3e4471320
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-09 09:33:13 +0700

    feat: add auth controller login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
     @Post('register')
-    @HttpCode(200)
     async register(@Body() createUserDto: CreateUserDto) {
         return this.authService.register(createUserDto);
+    }
+
+    @Post('/login')
+    async login(@Body() loginDto: LoginDto) {
+        return this.authService.login(loginDto)
     }
 }
