690896399e260366963952a2b45b3a13416a4ebb
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-09 08:35:44 +0700

    feat: add user service method findByEmail

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
@@ -12,5 +12,13 @@ export class UsersService {
   async create(userDto: CreateUserDto): Promise<User> {
     const createdUser = new this.userModel(userDto);
     return createdUser.save();
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.userModel.findOne({ email }).exec();
+    if (!user) {
+        return
+    }
+    return user
   }
 }
