737a81c95dd624b228198754df95e950d15496bc
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-09 08:36:25 +0700

    feat: add logic register if email user already exist

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
@@ -12,6 +12,15 @@ export class AuthService {
   ) {}
 
   async register(createUserDto: CreateUserDto) {
+    const userRegistered = await this.usersService.findByEmail(
+      createUserDto.email,
+    );
+    if (!!userRegistered) {
+      throw new HttpException(
+        'User already exists',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
     createUserDto.password = hashedPassword;
     const user = await this.usersService.create(createUserDto);
@@ -19,7 +28,7 @@ export class AuthService {
       name: user.name,
       email: user.email,
       roles: user.roles,
-      message: 'Your register is success'
+      message: 'Your register is success',
     };
     return result;
   }
