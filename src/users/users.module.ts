8e6969ea06a939c62dd8b359d9d9b96a1d7ae47e
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-09 10:07:20 +0700

    feat: add jwt service on user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtService } from '@nestjs/jwt';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([
-    {
-      name: 'User',
-      schema: UserSchema,
-    },
-  ])],
-  providers: [UsersService],
-  controllers: [UsersController]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  providers: [UsersService, JwtService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
