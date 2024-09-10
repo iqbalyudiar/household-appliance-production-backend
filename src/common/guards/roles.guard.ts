69ac072fe8e34eca0bff80c152bc5fa6f3ba5161
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-10 08:23:58 +0700

    feat: add logic roles guard and jwt guard

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,11 +1,25 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const requiredRoles = this.reflector.get<string[]>(
+      ROLES_KEY,
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    return requiredRoles.some((role) => role === user?.roles);
   }
 }
