4764ce04f92bcb06e26858b0de29322cc500f8ab
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-10 09:58:22 +0700

    feat: change structure response products

--- src/products/interface/product.interface.ts
+++ src/products/interface/product.interface.ts
@@ -1,7 +1,33 @@
-import { Document } from 'mongoose';
+import { Document, ObjectId } from 'mongoose';
+import { Product } from '../schemas/product.schema';
 
 export interface IProduct extends Document {
+  id: ObjectId;
   name: string;
   description: string;
   price: number;
 }
+
+export interface ISuccess {
+  success: boolean;
+}
+
+export interface IMessage {
+  message: string;
+}
+
+export interface IProductsResponse {
+  success: boolean;
+  products: Product[];
+  total: number;
+}
+
+export interface ID {
+  id: ObjectId | string | unknown;
+}
+
+export type IProductResponse = ISuccess & ID & Product;
+
+export type IProductCreateResponse = IProductResponse & IMessage;
+
+export type IProductDeleteResponse = ISuccess & IMessage;
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,19 +1,30 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Product } from './schemas/product.schema';
-import { IProduct } from './interface/product.interface';
+import {
+  IProduct,
+  IProductsResponse,
+  IProductResponse,
+  IProductCreateResponse,
+  IProductDeleteResponse,
+} from './interface/product.interface';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 @Injectable()
 export class ProductsService {
   constructor(@InjectModel('Product') private productModel: Model<IProduct>) {}
 
-  async findAll(): Promise<Product[]> {
-    return this.productModel.find().exec();
+  async findAll(): Promise<IProductsResponse> {
+    const products = await this.productModel.find().exec();
+    const total = await this.productModel.countDocuments().exec();
+    return {
+      success: true,
+      products,
+      total,
+    };
   }
 
-  async findOne(id: string): Promise<Product> {
+  async findOne(id: string): Promise<IProductResponse> {
     const product = await this.productModel.findById(id).exec();
     if (!product) {
       throw new HttpException(
@@ -24,18 +35,34 @@ export class ProductsService {
         HttpStatus.NOT_FOUND,
       );
     }
-    return product;
+    return {
+      success: true,
+      id: product._id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+    };
   }
 
-  async create(createProductDto: CreateProductDto): Promise<Product> {
+  async create(
+    createProductDto: CreateProductDto,
+  ): Promise<IProductCreateResponse> {
     const newProduct = new this.productModel(createProductDto);
-    return newProduct.save();
+    const product = await newProduct.save();
+    return {
+      success: true,
+      id: product._id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      message: 'Successfully create product',
+    };
   }
 
   async update(
     id: string,
     updateProductDto: UpdateProductDto,
-  ): Promise<Product> {
+  ): Promise<IProductCreateResponse> {
     const updatedProduct = await this.productModel
       .findByIdAndUpdate(id, updateProductDto, { new: true })
       .exec();
@@ -48,10 +75,17 @@ export class ProductsService {
         HttpStatus.NOT_FOUND,
       );
     }
-    return updatedProduct;
+    return {
+      success: true,
+      id: updatedProduct._id,
+      name: updatedProduct.name,
+      description: updatedProduct.description,
+      price: updatedProduct.price,
+      message: 'Successfully update product',
+    };
   }
 
-  async delete(id: string) {
+  async delete(id: string): Promise<IProductDeleteResponse> {
     const result = await this.productModel.findByIdAndDelete(id).exec();
     if (!result) {
       throw new HttpException(
