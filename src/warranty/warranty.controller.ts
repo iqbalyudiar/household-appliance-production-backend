52b48d2a060cbfdf6cd9a73c902329b3b3bb8d58
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-11 06:42:15 +0700

    feat: add create claim controller and service

--- src/warranty/warranty.controller.ts
+++ src/warranty/warranty.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Post, Request, Body, UseGuards } from '@nestjs/common';
+import { WarrantyService } from './warranty.service';
+import { CreateWarrantyClaimDto } from './dto/create-warranty-claim.dto';
+import { AuthGuard } from '@nestjs/passport';
 @Controller('warranty')
-export class WarrantyController {}
+export class WarrantyController {
+  constructor(private warrantyService: WarrantyService) {}
+
+  @Post('claim')
+  @UseGuards(AuthGuard('jwt'))
+  createClaim(@Body() createDto: CreateWarrantyClaimDto, @Request() req) {
+    return this.warrantyService.createClaim(req.user.id, createDto);
+  }
+}
--- src/warranty/warranty.service.ts
+++ src/warranty/warranty.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Warranty } from './schema/warranty.schema';
+import { CreateWarrantyClaimDto } from './dto/create-warranty-claim.dto';
+import { UpdateWarrantyClaimDto } from './dto/update-warranty-claim.dto';
 @Injectable()
-export class WarrantyService {}
+export class WarrantyService {
+  constructor(
+    @InjectModel(Warranty.name) private warrantyModel: Model<Warranty>,
+  ) {}
+
+  async createClaim(
+    userId: string,
+    createDto: CreateWarrantyClaimDto,
+  ): Promise<Warranty> {
+    const newClaim = new this.warrantyModel({
+      ...createDto,
+      user: userId,
+      issueDate: new Date(),
+      expiryDate: new Date(
+        new Date().setFullYear(new Date().getFullYear() + 1),
+      ),
+      isClaimed: true,
+      status: 'pending',
+    });
+    return newClaim.save();
+  }
+}
