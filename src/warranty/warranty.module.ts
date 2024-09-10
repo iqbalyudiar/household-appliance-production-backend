fc06dd5ff911411c3acef11c0d61eb6affb0580b
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-11 06:41:40 +0700

    feat: register warranty schema and guards on warranty module

--- src/warranty/warranty.module.ts
+++ src/warranty/warranty.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { WarrantyController } from './warranty.controller';
 import { WarrantyService } from './warranty.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { WarrantySchema } from './schema/warranty.schema';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Warranty',
+        schema: WarrantySchema,
+      },
+    ]),
+  ],
   controllers: [WarrantyController],
-  providers: [WarrantyService]
+  providers: [WarrantyService, JwtStrategy, RolesGuard, Reflector],
 })
 export class WarrantyModule {}
