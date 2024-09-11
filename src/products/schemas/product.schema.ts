5a1c3920182f6c87b069591de67f60c3b6dc9e9e
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-11 08:06:16 +0700

    feat: update product schema

--- src/products/schemas/product.schema.ts
+++ src/products/schemas/product.schema.ts
@@ -1,7 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 @Schema()
-export class Product {
+export class Product extends Document {
   @Prop({ required: true })
   name: string;
 
