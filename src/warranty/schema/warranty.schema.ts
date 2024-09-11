0a85e7e7bdea677a53922bbb561aa44e9de9cdda
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-11 08:19:11 +0700

    feat: update warranty schema

--- src/warranty/schema/warranty.schema.ts
+++ src/warranty/schema/warranty.schema.ts
@@ -1,29 +1,29 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import {Document, Types} from 'mongoose'
-import { Product } from 'src/products/schemas/product.schema'
-import { User } from 'src/users/schemas/user.schema'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { Product } from 'src/products/schemas/product.schema';
+import { User } from 'src/users/schemas/user.schema';
 
 export type WarrantyStatus = 'pending' | 'approved' | 'rejected';
 
 @Schema()
 export class Warranty extends Document {
-    @Prop({ type: Types.ObjectId, ref: 'Product' })
-    product: Product;
+  @Prop({ type: Types.ObjectId, ref: 'Product', required: true })
+  product: Product;
 
-    @Prop({ type: Types.ObjectId, ref: 'User' })
-    user: User;
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  user: User;
 
-    @Prop()
-    issueDate: Date;
+  @Prop()
+  issueDate: Date;
 
-    @Prop()
-    expiryDate: Date;
+  @Prop()
+  expiryDate: Date;
 
-    @Prop({ default: false })
-    isClaimed: boolean;
+  @Prop({ default: false })
+  isClaimed: boolean;
 
-    @Prop({ default: 'pending' })
-    status: WarrantyStatus;
+  @Prop({ default: 'pending' })
+  status: WarrantyStatus;
 }
 
 export const WarrantySchema = SchemaFactory.createForClass(Warranty);
